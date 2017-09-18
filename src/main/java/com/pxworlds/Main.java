a4c710f2271c30317bd064abc106efd823570307
Temmi Pietsch <welcome@temmi.land>
2017-09-18 19:49:59 +0200

    Added JavaDoc comments and restructured repository

--- src/main/java/com/pxworlds/Main.java
+++ src/main/java/com/pxworlds/Main.java
@@ -3,9 +3,13 @@ package com.pxworlds;
 
 public class Main {
 
+    private static final String applicationName = "pxWorlds - Let's adventure!";
+    private static final int applicationWidth = 1280;
+    private static final int applicationHeight = 720;
+
     public static void main(String[] args) {
         SharedLibraryLoader.load();
-        new pxWorlds(1280, 720).run();
+        new pxWorlds(applicationWidth, applicationHeight, applicationName).run();
     }
 
 }
