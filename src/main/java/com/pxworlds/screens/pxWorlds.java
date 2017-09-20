7422f9cc9ac3069e1f49eff005df4bdb6d65e455
Temmi Pietsch <welcome@temmi.land>
2017-09-20 19:37:54 +0200

    Removed LWJGL 3, Added new structure

---
+++ src/main/java/com/pxworlds/screens/pxWorlds.java
@@ -0,0 +1,34 @@
+package com.pxworlds.screens;
+
+import javax.swing.*;
+
+public class pxWorlds extends JFrame {
+
+    private String TITLE;
+    private int WIDTH;
+    private int HEIGHT;
+    private boolean FULLSCREEN;
+
+    public pxWorlds(String title, int width, int height, boolean fullscreen) {
+
+        super(title);
+
+        this.TITLE = title;
+        this.WIDTH = width;
+        this.HEIGHT = height;
+        this.FULLSCREEN = fullscreen;
+
+        setSize(WIDTH, HEIGHT);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+        if(fullscreen) {
+            setExtendedState(JFrame.MAXIMIZED_BOTH);
+            setUndecorated(true);
+        }
+
+        setVisible(true);
+
+    }
+
+}
