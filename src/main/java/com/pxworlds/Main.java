bb0a00da87c65df03be37d15aaa16e15b3253b02
tomxpcvx <dev@tomxpcvx.wtf>
2017-09-20 20:16:01 +0200

    Added screen dimensions and cleaned  up formatting

--- src/main/java/com/pxworlds/Main.java
+++ src/main/java/com/pxworlds/Main.java
@@ -2,14 +2,22 @@ package com.pxworlds;
 
 import com.pxworlds.screens.PxWorlds;
 
+import java.awt.*;
+
 public class Main {
 
-    private static final String TITLE = "PxWorlds - Let's Adventure!";
-    private static final int WIDTH = 1920;
-    private static final int HEIGHT = 1080;
+    private static final String  TITLE      = "PxWorlds - Let's Adventure!";
+    private static       int     WIDTH      = 1920;
+    private static       int     HEIGHT     = 1080;
     private static final boolean FULLSCREEN = true;
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        int       width      = (int) screenSize.getWidth();
+        int       height     = (int) screenSize.getHeight();
+        WIDTH = width;
+        HEIGHT = height;
+
         new PxWorlds(TITLE, WIDTH, HEIGHT, FULLSCREEN);
     }
 
