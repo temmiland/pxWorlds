9d0c4edae57696dfe7b2d0a051d4573b3a1ab11f
Temmi Pietsch <welcome@temmi.land>
2017-09-22 10:09:34 +0200

    Created profiles config for later purposes

---
+++ src/main/java/com/pxworlds/configuration/ProfileConfiguration.java
@@ -0,0 +1,43 @@
+package com.pxworlds.configuration;
+
+import java.util.UUID;
+
+public class ProfileConfiguration extends Configuration {
+
+    private String profileName = "";
+    private String userName    = "";
+    private UUID   uuid        = UUID.randomUUID();
+
+    public ProfileConfiguration(String profileName, String userName, UUID uuid) {
+        this.profileName = profileName;
+        this.userName = userName;
+        this.uuid = uuid;
+    }
+
+    public String getProfileName() {
+        return profileName;
+    }
+
+    public ProfileConfiguration setProfileName(String profileName) {
+        this.profileName = profileName;
+        return this;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public ProfileConfiguration setUserName(String userName) {
+        this.userName = userName;
+        return this;
+    }
+
+    public UUID getUuid() {
+        return uuid;
+    }
+
+    public ProfileConfiguration setUuid(UUID uuid) {
+        this.uuid = uuid;
+        return this;
+    }
+}
