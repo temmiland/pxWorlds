8e006c8d8eb7baa3919c7208fecaff20e195e8aa
tomxpcvx <dev@tomxpcvx.wtf>
2018-07-26 00:13:50 +0200

    Added logo to main screen

--- src/main/java/com/pxworlds/game/gui/MenuGui.java
+++ src/main/java/com/pxworlds/game/gui/MenuGui.java
@@ -10,10 +10,10 @@ public class MenuGui extends Gui {
     public MenuGui(Window window, String tileSheetTexture, int amountOfTiles) {
         super(window, tileSheetTexture, amountOfTiles);
 
-        Logo logo = new Logo(new Vector2f(0, 100), new Vector2f(144, 48));
-        Button playButton = new Button(new Vector2f(0, 0), new Vector2f(200, 45));
-        Button optionButton = new Button(new Vector2f(0, -95), new Vector2f(200, 45));
-        Button exitButton = new Button(new Vector2f(0, -190), new Vector2f(200, 45));
+        Logo logo = new Logo(new Vector2f(0, 250), new Vector2f(64, 64));
+        Button playButton = new Button(new Vector2f(0, -50), new Vector2f(200, 45));
+        Button optionButton = new Button(new Vector2f(0, -145), new Vector2f(200, 45));
+        Button exitButton = new Button(new Vector2f(0, -240), new Vector2f(200, 45));
 
         registerLogos(logo);
         registerButtons(playButton, optionButton, exitButton);
