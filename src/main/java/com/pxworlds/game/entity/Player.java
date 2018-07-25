c3cead06052165c7119630fb9d213ba04f625df2
tomxpcvx <dev@tomxpcvx.wtf>
2018-07-25 18:40:14 +0200

    Added player movement speed (running and walking)

--- src/main/java/com/pxworlds/game/entity/Player.java
+++ src/main/java/com/pxworlds/game/entity/Player.java
@@ -13,24 +13,32 @@ public class Player extends Entity {
 	public static final int ANIM_IDLE = 0;
 	public static final int ANIM_WALK = 1;
 	public static final int ANIM_SIZE = 2;
-	
-	public Player(Transform transform) {
+    private int movementSpeed = 5;
+
+    public Player(Transform transform) {
 		super(ANIM_SIZE, transform);
-		setAnimation(ANIM_IDLE, new Animation(4, 2, "player/idle"));
-		setAnimation(ANIM_WALK, new Animation(4, 2, "player/walking"));
+		setAnimation(ANIM_IDLE, new Animation(4, 3, "player/idle"));
+		setAnimation(ANIM_WALK, new Animation(4, 8, "player/walking"));
 	}
+
+	public void setMovementSpeed(MovementSpeed movementSpeed) {
+        this.movementSpeed = movementSpeed.getSpeedValue();
+    }
 	
 	@Override
 	public void update(float delta, Window window, Camera camera, World world) {
 		Vector2f movement = new Vector2f();
-		if (window.getInput().isKeyDown(GLFW.GLFW_KEY_A)) movement.add(-10 * delta, 0);
+
+        if (window.getInput().isKeyDown(GLFW.GLFW_KEY_A)) movement.add(-movementSpeed * delta, 0);
 		
-		if (window.getInput().isKeyDown(GLFW.GLFW_KEY_D)) movement.add(10 * delta, 0);
+		if (window.getInput().isKeyDown(GLFW.GLFW_KEY_D)) movement.add(movementSpeed * delta, 0);
 		
-		if (window.getInput().isKeyDown(GLFW.GLFW_KEY_W)) movement.add(0, 10 * delta);
+		if (window.getInput().isKeyDown(GLFW.GLFW_KEY_W)) movement.add(0, movementSpeed * delta);
 		
-		if (window.getInput().isKeyDown(GLFW.GLFW_KEY_S)) movement.add(0, -10 * delta);
-		
+		if (window.getInput().isKeyDown(GLFW.GLFW_KEY_S)) movement.add(0, -movementSpeed * delta);
+
+        setMovementSpeed(window.getInput().isKeyDown(GLFW.GLFW_KEY_LEFT_SHIFT) ? MovementSpeed.RUNNING : MovementSpeed.WALKING);
+
 		move(movement);
 		
 		if (movement.x != 0 || movement.y != 0)
