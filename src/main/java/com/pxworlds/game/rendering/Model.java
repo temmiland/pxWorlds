90ac2b370e9b631e1a603331da902013aaffe5f8
Temmi Pietsch <welcome@temmi.land>
2025-11-09 18:54:27 +0100

    ♻️ replace finalize() with AutoCloseable in Model
    - Implement AutoCloseable interface for proper resource cleanup
    - Remove deprecated finalize() method
    - Ensures OpenGL buffers are properly deleted

--- src/main/java/com/pxworlds/game/rendering/Model.java
+++ src/main/java/com/pxworlds/game/rendering/Model.java
@@ -9,66 +9,66 @@ import java.nio.IntBuffer;
 
 import org.lwjgl.BufferUtils;
 
-public class Model {
+public class Model implements AutoCloseable {
 	private int drawCount;
-	
+
 	private int vertexObject;
 	private int textureCoordObject;
-	
+
 	private int indexObject;
-	
+
 	public Model(float[] vertices, float[] tex_coords, int[] indices) {
 		drawCount = indices.length;
-		
+
 		vertexObject = glGenBuffers();
 		glBindBuffer(GL_ARRAY_BUFFER, vertexObject);
 		glBufferData(GL_ARRAY_BUFFER, createBuffer(vertices), GL_STATIC_DRAW);
-		
+
 		textureCoordObject = glGenBuffers();
 		glBindBuffer(GL_ARRAY_BUFFER, textureCoordObject);
 		glBufferData(GL_ARRAY_BUFFER, createBuffer(tex_coords), GL_STATIC_DRAW);
-		
+
 		indexObject = glGenBuffers();
 		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexObject);
-		
+
 		IntBuffer buffer = BufferUtils.createIntBuffer(indices.length);
 		buffer.put(indices);
 		buffer.flip();
-		
+
 		glBufferData(GL_ELEMENT_ARRAY_BUFFER, buffer, GL_STATIC_DRAW);
 
 		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 		glBindBuffer(GL_ARRAY_BUFFER, 0);
 	}
 
-	protected void finalize() throws Throwable {
+	@Override
+	public void close() {
 		glDeleteBuffers(vertexObject);
 		glDeleteBuffers(textureCoordObject);
 		glDeleteBuffers(indexObject);
-		super.finalize();
 	}
-	
+
 	public void render() {
 		glEnableVertexAttribArray(0);
 		glEnableVertexAttribArray(1);
 
 		glBindBuffer(GL_ARRAY_BUFFER, vertexObject);
 		glVertexAttribPointer(0, 3, GL_FLOAT, false, 0, 0);
-		
+
 		glBindBuffer(GL_ARRAY_BUFFER, textureCoordObject);
 		glVertexAttribPointer(1, 2, GL_FLOAT, false, 0, 0);
-		
+
 		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexObject);
 		glDrawElements(GL_TRIANGLES, drawCount, GL_UNSIGNED_INT, 0);
-		
+
 		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 		glBindBuffer(GL_ARRAY_BUFFER, 0);
 
 		glDisableVertexAttribArray(0);
 		glDisableVertexAttribArray(1);
-		
+
 	}
-	
+
 	private FloatBuffer createBuffer(float[] data) {
 		FloatBuffer buffer = BufferUtils.createFloatBuffer(data.length);
 		buffer.put(data);
